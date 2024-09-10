eca182f3a571469400158e0b1b9d6616e98e38a4
Tomas.K <tomas.konopackis@gmail.com>
2024-09-10 15:31:34 +0300

    updated with location list and its separate files

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { Footer } from "../components/footer/Footer.jsx";
+import { Header } from "../components/header/Header.jsx";
+import { HomeHero } from "../components/hero/HomeHero.jsx";
+
+export function Home() {
+  return (
+    <>
+      <Header/>
+      <HomeHero />
+      <Footer/>
+    </>
+  )
+}
+
